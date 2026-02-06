bdb6975ac7046573d13e6e45b291f5861fed2a1b
Jane Years <janeletoxx@gmail.com>
2026-02-07 01:32:16 +0200

    add basic  style.css

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "./router";
 import "./styles.css";
 
+
 const container = document.getElementById("root");
 if (!container) throw new Error("Root element not found");
 const root: Root = createRoot(container);
