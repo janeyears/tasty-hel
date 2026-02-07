86bda545a6c633041c1ecd04fd2653b29c7a8269
Anton Zobnin <nurgling96@gmail.com>
2026-02-08 01:19:39 +0200

    add react types

--- src/pages/PlacePage.tsx
+++ src/pages/PlacePage.tsx
@@ -1,7 +1,8 @@
-import { useParams } from "react-router-dom"
+import React from "react";
+import { useParams } from "react-router-dom";
 
 export default function PlacePage() {
-  const { id } = useParams()
+  const { id } = useParams();
 
-  return <div>Restaurant {id}</div>
+  return <div>Restaurant {id}</div>;
 }
