05d971dd237afb1975834fd4f59c28f8d79ec991
Anton Zobnin <nurgling96@gmail.com>
2026-02-08 01:57:35 +0200

    WIP on add-linters (#24)

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { createRoot, type Root } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router";
+import "./styles.css";
 
 const container = document.getElementById("root");
 if (!container) throw new Error("Root element not found");
