d64e82c25ee5ff13e75a3ad6b4aadc92e564a76c
Jane Years <janeletoxx@gmail.com>
2026-06-16 15:04:26 +0300

    main page return

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const HomePage = () => {
-  return <h1 className="text-5xl font-bold">Tailwind works!</h1>;
+  return <div>HomePage</div>;
 };
 
 export default HomePage;
