7ddfa83da8399bcbbce7822d8633b402a8381ab8
Jane Years <janeletoxx@gmail.com>
2026-06-20 16:37:49 +0300

    make Hero component reusable

--- src/components/layout/Hero.tsx
+++ src/components/layout/Hero.tsx
@@ -1,17 +1,24 @@
 import React from 'react';
 
-export function Hero() {
+interface HeroProps {
+  title: string;
+  image: string;
+  imageAlt?: string;
+};
+
+
+export function Hero({ title, image, imageAlt }: HeroProps) {
   return (
     <section className="border-b-2 border-primary">
       <div className="mx-auto max-w-7xl px-8 py-12">
         <div className="grid grid-cols-2 items-center gap-12">
           <h1 className="text-7xl italic font-regular text-primary">
-            my personal recommendations from places where I’ve been
+            {title}
           </h1>
 
           <img
-            src="src/assets/main-hero.jpg"
-            alt="Main page image"
+            src={image}
+            alt={imageAlt}
             className="w-full rounded-3xl object-cover border-2 border-primary"
           />
         </div>
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,7 +4,7 @@ import { Hero } from '../components/layout/Hero';
 const HomePage = () => {
   return (
     <div>
-      <Hero />
+      <Hero title="my personal recommendations from places where I’ve been" image="src/assets/main-hero.jpg" imageAlt="Main page image" />
     </div>
   );
 };
